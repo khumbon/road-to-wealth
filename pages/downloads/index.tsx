b9e82980aa2d66a18eb5905f8cfda1ddf40274fc
Khumbo Nyirenda <khumbon@gmail.com>
2022-10-03 22:19:02 +0100

    Added budget download

--- pages/downloads/index.tsx
+++ pages/downloads/index.tsx
@@ -1,11 +1,15 @@
+/* eslint-disable react/no-unknown-property */
 import React from 'react';
 import { Heading } from '../../components';
+import ComputerOnDeskBlankBudget from '../../public/assets/computer-on-desk-blank-budget.png'
 
 const Downloads = () => {
   return (
     <div>
       <Heading>Downloads</Heading>
-      Hold tight, a downloads page will be here soon
+      <a href="/assets/downloads/budget.xlsx" target="_blank" download="Budget.xlsx">
+        <img src={ComputerOnDeskBlankBudget.src} alt="Image of a budget" />
+      </a>
     </div>
   );
 };
