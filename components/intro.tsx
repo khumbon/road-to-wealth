3a5d6c3a7fddf9b2085744fbeafa17084ed2911d
Khumbo Nyirenda <khumbon@gmail.com>
2025-03-05 19:21:23 +0000

    fix: removed comments

--- components/intro.tsx
+++ components/intro.tsx
@@ -8,7 +8,6 @@ import Image from 'next/image';
 import { colours } from '../constants/colours';
 
 const Intro = () => {
-  //** <div style={{width: '1920px', height: '1000px', backgroundImage: 'url(/assets/blog/images/Banner2.png)', backgroundRepeat: 'no-repeat', clip:'rect(350px,1920px,952px,0px)', position:'absolute'}}/>  */
   return (
     <div>
       <Grid
@@ -46,10 +45,3 @@ const Intro = () => {
 };
 
 export default Intro;
-/**<div style={{position: 'relative', width: '100%', height: 0, paddingTop: '60.0000%',
-paddingBottom: '48px', boxShadow: '0 2px 8px 0 rgba(63,69,81,0.16)', marginTop: '1.6em', marginBottom: '0.9em', overflow: 'hidden',
-borderRadius: '8px', willChange: 'transform'}}>
-<iframe loading="lazy" style={{ position: 'absolute', width: '100%', height: '100%', top: 0, left: 0, border: 'none', padding: 0, margin: 0,}}
-    src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAEpUj2ziOA&#x2F;view?embed"  allow="fullscreen">
-  </iframe>
-</div>*/
